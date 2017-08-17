5f541f6f6560d5ad33abfa9d08d4ba6d4d4f67f6
Tiberiu Tesileanu <ttesileanu@gmail.com>
2017-08-17 13:26:19 -0400

    Fixed pacakge's __init__ so that statistics module gets loaded.

--- __init__.py
+++ __init__.py
@@ -1,19 +1,22 @@
-from multicov import alphabet
-from multicov import alignment
-from multicov import binary
 from multicov import align_io
+from multicov import alignment
+from multicov import alphabet
+from multicov import binary
 from multicov import filtering
+from multicov import statistics
 
-# noinspection PyUnresolvedReferences
-from multicov.alphabet import *
-# noinspection PyUnresolvedReferences
-from multicov.alignment import *
-# noinspection PyUnresolvedReferences
-from multicov.binary import *
 # noinspection PyUnresolvedReferences
 from multicov.align_io import *
 # noinspection PyUnresolvedReferences
+from multicov.alignment import *
+# noinspection PyUnresolvedReferences
+from multicov.alphabet import *
+# noinspection PyUnresolvedReferences
+from multicov.binary import *
+# noinspection PyUnresolvedReferences
 from multicov.filtering import *
+# noinspection PyUnresolvedReferences
+from multicov.statistics import *
 
 __package__ = 'multicov'
-__all__ = ['alphabet', 'alignment', 'binary', 'align_io', 'filtering']
+__all__ = ['alphabet', 'alignment', 'binary', 'align_io', 'filtering', 'statistics']
